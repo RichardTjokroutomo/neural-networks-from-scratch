c1d7d5e77c0db856688a43a42780663b032495e6
Richard Tjokroutomo <richard.tjokro2@gmail.com>
2023-06-07 15:37:55 +0700

    tenth commit, fixed some stuff

--- mnist_loader.py
+++ mnist_loader.py
@@ -1,22 +1,10 @@
-"""
-mnist_loader
-~~~~~~~~~~~~
 
-A library to load the MNIST image data.  For details of the data
-structures that are returned, see the doc strings for ``load_data``
-and ``load_data_wrapper``.  In practice, ``load_data_wrapper`` is the
-function usually called by our neural network code.
-"""
-
-#### Libraries
-# Standard library
 import pickle
 import gzip
-
-# Third-party libraries
 import numpy as np
 
 # path
+# example: my root path: C:/Users/Richard/Documents/-projects/project-17
 path = "ROOT_PATH/data/mnist.pkl.gz"
 
 
@@ -25,11 +13,11 @@ def load_data():
     f = gzip.open(path, "rb")
     training_data, validation_data, test_data = pickle.load(f, encoding="iso-8859-1")
     f.close()
-    return (training_data, test_data)
+    return (training_data, validation_data, test_data)
 
 def load_mnist():
    
-    tr_d, te_d = load_data()
+    tr_d, va_d, te_d = load_data()
     training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
     training_results = [vectorized_result(y) for y in tr_d[1]]
     training_data = list(zip(training_inputs, training_results))
